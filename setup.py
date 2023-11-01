abebc83cb3e4048144eccd186377135535f2f654
zhatz111 <62628320+zhatz111@users.noreply.github.com>
2023-10-31 22:08:41 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project is meant to serve as a dashboard for FAO data and help showcase my skills in the data science realm.',
+    author='Zach Hatzenbeller',
+    license='MIT',
+)
